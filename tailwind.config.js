76eebcce0b285485dd695147fc7d3fa016440fbd
bipul-hossein <bipul.pgmr@gmail.com>
2024-12-31 17:59:36 +0600

    footer added and primary color change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
-export default {
-  content: ["./views/*.ejs"],
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./views/**/*.ejs",
+    // add other template paths as needed
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#00796B",
+          light: "#26A69A",
+          dark: "#004D40",
+        },
+        secondary: "#FFA000",
+        accent: "#FF5722",
+      },
+    },
   },
-  plugins: [require("tailwindcss"), require("autoprefixer")],
+  plugins: [],
 };
